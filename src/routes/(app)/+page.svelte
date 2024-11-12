de687887a055eadf956294a0e842a1f28dac7886
momanyi <momanyi@momanyis-MacBook-Air.local>
2024-11-12 15:11:33 +0300

    Create Website Navigation Menu #1
    Created initial layout for the top navigation

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	export let data;
-	const { greeting } = data.settings[0];
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<h1 class="text-lg">{greeting}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<Header/>
+</div>
