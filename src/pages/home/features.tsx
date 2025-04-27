000b664d1cad73118a48461688aa85b27b184197
mwabzy <eb5013667@gmail.com>
2025-04-27 22:44:58 +0300

    paddingandspacingchanges

--- src/pages/home/features.tsx
+++ src/pages/home/features.tsx
@@ -11,21 +11,21 @@ const Features = () => {
     <div className="grid gap-20  md:grid-cols-3 text-center">
       
       {/* Card 1 */}
-      <div className="flex flex-col items-center space-y-4 shadow-xl p-10">
+      <div className="flex flex-col items-center space-y-2 shadow-xl p-6">
         <img src={doctor} alt="Icon 1" className="h-12 w-12" />
         <h3 className="text-lg  font-semibold text-gray-800"> Find a Doctor</h3>
         
       </div>
 
       {/* Card 2 */}
-      <div className="flex flex-col items-center space-y-4 shadow-xl p-10">
+      <div className="flex flex-col items-center space-y-2 shadow-xl p-6">
           <img src= {patient} alt="Icon 2" className="h-12 w-12" />
         <h3 className="text-lg font-semibold text-gray-800"> Patient Registration</h3>
         
       </div>
 
       {/* Card 3 */}
-      <div className="flex flex-col items-center space-y-4 shadow-xl p-10">
+      <div className="flex flex-col items-center space-y-2 shadow-xl p-6">
         <img src={location} alt="Icon 3" className="h-12 w-12" />
         <h3 className="text-lg font-semibold text-gray-800"> Getting to the Hospital</h3>
        
