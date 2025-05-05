a84caf3ea02d57a4c2bef0c791a9cfe0c65600e4
mwabzy <eb5013667@gmail.com>
2025-05-05 00:35:21 -0700

    Team Page Update 4

--- src/pages/clinics/ServicesCards.tsx
+++ src/pages/clinics/ServicesCards.tsx
@@ -3,32 +3,32 @@ import { useState } from "react";
 import { Link } from "react-router";
 import clinicalServices from "@/data/clinicalServices.json";
 
-type AccordionItem = {
-  title: string;
-  Subject: string;
-};
+// type AccordionItem = {
+//   title: string;
+//   Subject: string;
+// };
 
-type Clinic = {
-  id: string;
-  name: string;
-  description: string;
-  image: {
-    src: string;
-    alt: string;
-  };
-};
+// type Clinic = {
+//   id: string;
+//   name: string;
+//   description: string;
+//   image: {
+//     src: string;
+//     alt: string;
+//   };
+// };
 
-type Service = {
-  id: string;
-  name: string;
-  description: string;
-  image: {
-    src: string;
-    alt: string;
-  };
-  accordionItems?: Record<string, AccordionItem>;
-  clinics?: Clinic[];
-};
+// type Service = {
+//   id: string;
+//   name: string;
+//   description: string;
+//   image: {
+//     src: string;
+//     alt: string;
+//   };
+//   accordionItems?: Record<string, AccordionItem>;
+//   clinics?: Clinic[];
+// };
 
 // Cardtitles
 const ServiceCards = () => {
